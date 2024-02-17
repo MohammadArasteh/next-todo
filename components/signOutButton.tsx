de9b7c1d1400eab5bd8153d3ecc2be472044084c
Mohammad Arasteh <mohammadkb79@Gmail.com>
2024-02-18 01:57:10 +0330

    [feat][client] Clear local storage while logging out

--- components/signOutButton.tsx
+++ components/signOutButton.tsx
@@ -1,10 +1,17 @@
+"use client";
+
+import localStorageInstance from "@/lib/localStorage";
 import { signOut } from "@/lib/server-actions/auth";
 import { Text } from "@mantine/core";
 
 export default function SignOutButton() {
   return (
     <form action={signOut}>
-      <button type="submit" style={{ all: "unset" }}>
+      <button
+        type="submit"
+        style={{ all: "unset" }}
+        onClick={() => localStorageInstance.clearAll()}
+      >
         <Text style={{ cursor: "pointer" }}>SIGN OUT</Text>
       </button>
     </form>
