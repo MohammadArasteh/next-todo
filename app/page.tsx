77366d527a855ab9dfb6f10e6b2430b0e239a496
Mohammad Arasteh <mohammadkb79@Gmail.com>
2024-02-17 23:34:50 +0330

    [feat][ui] Implement root page

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,20 @@ import classes from "./page.module.css";
 export default function HomePage() {
   return (
     <Box className={classes.container}>
+      <Image
+        src={"/girl-work-on-laptop.png"}
+        alt="main page image"
+        width={500}
+        height={500}
+      />
+      <Text size="xl" c="#c6cdd5">
+        Step into a world of seamless productivity with our Next.js-powered
+        to-do app. Experience the thrill of swift task management through a
+        visually stunning interface. Real-time updates keep you in control,
+        while responsive design ensures a dynamic experience on any device.
+        Elevate your to-do list with Next.js – where efficiency meets
+        excitement!
+      </Text>
     </Box>
   );
 }
