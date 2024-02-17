299bbb30051ec91e8e78f3b6a7b717bd81d89c78
Mohammad Arasteh <mohammadkb79@Gmail.com>
2024-02-17 23:34:54 +0330

    [feat][client] Implement new list modal route

---
+++ app/dashboard/@modal/new/page.tsx
@@ -0,0 +1,85 @@
+"use client";
+
+import React from "react";
+import { createList } from "@/lib/server-actions/todo";
+import { useTodoList } from "@/store/zustand";
+import { Button, Divider, Group, Modal, TextInput } from "@mantine/core";
+import { useForm } from "@mantine/form";
+import { useDisclosure } from "@mantine/hooks";
+import { useRouter } from "next/navigation";
+
+type FormValues = {
+  title: string;
+};
+
+export default function New() {
+  const [, { close }] = useDisclosure(false);
+  const router = useRouter();
+
+  const form = useForm<FormValues>({
+    initialValues: {
+      title: "",
+    },
+
+    validate: {
+      title: (value) => (!value ? "* Title is required" : null),
+    },
+  });
+  const addList = useTodoList((store) => store.addList);
+  const [isSubmitting, setIsSubmitting] = React.useState<boolean>(false);
+
+  const onSubmit = async (values: FormValues) => {
+    setIsSubmitting(true);
+    const result = await createList(values);
+    const { error, data } = JSON.parse(result);
+    if (error?.message) console.error(error.message);
+    else if (data) {
+      addList({
+        id: data.id,
+        createdBy: data.created_by,
+        title: data.title,
+      });
+      router.push(`/dashboard/todo/${data.id}`);
+      onClose();
+    }
+    setIsSubmitting(false);
+  };
+
+  const onClose = () => {
+    router.back();
+    close();
+  };
+
+  return (
+    <Modal
+      opened={true}
+      onClose={onClose}
+      title="New Todo List"
+      centered
+      overlayProps={{
+        backgroundOpacity: 0.55,
+        blur: 3,
+      }}
+      transitionProps={{
+        transition: "fade",
+        duration: 100,
+        timingFunction: "linear",
+      }}
+    >
+      <form onSubmit={form.onSubmit(onSubmit)}>
+        <TextInput
+          withAsterisk
+          label="Title"
+          placeholder="task title"
+          {...form.getInputProps("title")}
+        />
+        <Divider my="md" />
+        <Group justify="flex-end" mt="md">
+          <Button loading={isSubmitting} type="submit">
+            Add
+          </Button>
+        </Group>
+      </form>
+    </Modal>
+  );
+}
