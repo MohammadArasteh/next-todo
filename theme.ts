3170ff030a38ca238a393b3195ef96635615a080
Mohammad Arasteh <mohammadkb79@Gmail.com>
2024-02-17 23:34:50 +0330

    [feat] Add mantine theme provider

---
+++ theme.ts
@@ -0,0 +1,14 @@
+"use client";
+
+import { createTheme } from "@mantine/core";
+
+export const theme = createTheme({
+  cursorType: "pointer",
+  breakpoints: {
+    xs: "30em",
+    sm: "48em",
+    md: "64em",
+    lg: "74em",
+    xl: "90em",
+  },
+});
