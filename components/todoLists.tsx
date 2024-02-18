7c47b7e6eae3679c4bc464c250b94e83fe15646d
Mohammad Arasteh <mohammadkb79@Gmail.com>
2024-02-18 04:01:41 +0330

    [fix][client] Display proper text in todo lists when list is empty

--- components/todoLists.tsx
+++ components/todoLists.tsx
@@ -2,9 +2,10 @@
 
 import React from "react";
 import { useTodoList } from "@/store/zustand";
-import { Flex } from "@mantine/core";
+import { Flex, Text } from "@mantine/core";
 import TodoListItem from "./todoListItem";
 import { Client } from "../models";
+import { IoIosAddCircle } from "react-icons/io";
 
 type Props = {
   onListClick: (list: Client.Todo.TodoList) => void;
@@ -14,13 +15,24 @@ const TodoLists = (props: Props) => {
   const lists = useTodoList((store) => store.lists);
   return (
     <Flex direction={"column"}>
-      {lists.map((list, index) => (
-        <TodoListItem
-          key={index}
-          list={list}
-          onClick={() => props.onListClick(list)}
-        />
-      ))}
+      {lists.length ? (
+        lists.map((list, index) => (
+          <TodoListItem
+            key={index}
+            list={list}
+            onClick={() => props.onListClick(list)}
+          />
+        ))
+      ) : (
+        <Text px="sm">
+          {"You don't have any list!"}
+          <br /> start creating a list from
+          <IoIosAddCircle
+            style={{ display: "inline", margin: "0px 0px 0px 5px" }}
+          />{" "}
+          icon which is located at top-right section of this panel.
+        </Text>
+      )}
     </Flex>
   );
 };
